157006877bf18d66c8356e914d757450be8b267d
Miruna Serian <zcbtser@ucl.ac.uk>
2019-06-04 12:28:24 +0100

    reorganised the script

--- seq_fetcher.py
+++ seq_fetcher.py
@@ -7,11 +7,8 @@ page_link = "http://www.imgt.org/3Dstructure-DB/cgi/details.cgi?pdbcode=1A2Y"
 page_response = requests.get(page_link, timeout=5)
 soup = BeautifulSoup(page_response.text, "html.parser")
 
-#-----------------------------------------------------------------------------------------------------------------------
-#                                        Light Chain
-#-----------------------------------------------------------------------------------------------------------------------
-def get_light_chain(soup):
-    the_td = soup.find_all('td', class_ = "data_l")
+def get_chain(the_class):
+    the_td = soup.find_all('td', class_ = the_class)
     for td in the_td:
         the_text = td.text
         p = re.compile(r'(.+\s+]\s+.+)Sequence\sin\sFASTA')
@@ -22,27 +19,12 @@ def get_light_chain(soup):
             linked_chain = re.sub(r'[\\n\s+\]\\n]', '',chain_seq)
             light_chain = str(linked_chain).replace('xa0', '')
             return(light_chain)
-#-----------------------------------------------------------------------------------------------------------------------
-#                                        Heavy Chain
-#-----------------------------------------------------------------------------------------------------------------------
+
+def get_light_chain(soup):
+    return(get_chain("data_l"))
 
 def get_heavy_chain(soup):
-    the_td = soup.find_all('td', class_ = "data_h")
-    for td in the_td:
-        the_text = td.text
-        p = re.compile(r'(.+\s+]\s+.+)Sequence\sin\sFASTA')
-        the_link = p.finditer(the_text)
-        for match in the_link:
-            sequence = match.groups()
-            chain_seq= str(sequence)
-            linked_chain = re.sub(r'[\\n\s+\]\\n]', '',chain_seq)
-            heavy_chain = str(linked_chain).replace('xa0', '')
-            return(heavy_chain)
-
-
-
+    return(get_chain("data_h"))
 
 print(get_light_chain(soup))
 print(get_heavy_chain(soup))
-
-
